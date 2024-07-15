07abec852619a31ecb64b3c44c1b9b70bf73f968
Bogoljub Simonovic <bogoljub2012@gmail.com>
2024-07-15 09:13:09 +0200

    register,logout,login route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,8 +11,8 @@ import { createBrowserRouter } from "react-router-dom";
 // import GenreList from "./pages/Admin/GenreList.jsx";
 
 // // Restricted
-// import Login from "./pages/Auth/Login.jsx";
-// import Register from "./pages/Auth/Register.jsx";
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
 // import PrivateRoute from "./pages/Auth/PrivateRoute.jsx";
 
 import Home from "./pages/Home.jsx";
@@ -29,10 +29,10 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<Home />} />
-      {/* <Route path="/movies" element={<AllMovies />} />
       <Route path="/login" element={<Login />} />
+      {/* <Route path="/movies" element={<AllMovies />} /> */}
       <Route path="/register" element={<Register />} />
-      <Route path="/movies/:id" element={<MovieDetails />} /> */}
+      {/* <Route path="/movies/:id" element={<MovieDetails />} /> */}
 
       {/* <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
