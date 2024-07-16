f20db52a3f7151b510ab4464278b78eb0940cedf
Bogoljub Simonovic <bogoljub2012@gmail.com>
2024-07-16 12:42:03 +0200

    update and delete movie

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,8 +17,8 @@ import PrivateRoute from "./pages/Auth/PrivateRoute.jsx";
 
 import Home from "./pages/Home.jsx";
 import Profile from "./pages/User/Profile.jsx";
-// import AdminMoviesList from "./pages/Admin/AdminMoviesList.jsx";
-// import UpdateMovie from "./pages/Admin/UpdateMovie.jsx";
+import AdminMoviesList from "./pages/Admin/AdminMoviesList.jsx";
+import UpdateMovie from "./pages/Admin/UpdateMovie.jsx";
 import CreateMovie from "./pages/Admin/CreateMovie.jsx";
 // import AllMovies from "./pages/Movies/AllMovies.jsx";
 // import MovieDetails from "./pages/Movies/MovieDetails.jsx";
@@ -41,8 +41,8 @@ const router = createBrowserRouter(
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/movies/genre" element={<GenreList />} />
         <Route path="/admin/movies/create" element={<CreateMovie />} />
-        {/* <Route path="/admin/movies-list" element={<AdminMoviesList />} /> */}
-        {/* <Route path="/admin/movies/update/:id" element={<UpdateMovie />} /> */}
+        <Route path="/admin/movies-list" element={<AdminMoviesList />} />
+        <Route path="/admin/movies/update/:id" element={<UpdateMovie />} />
         {/* <Route path="/admin/movies/dashboard" element={<AdminDashboard />} /> */}
         {/* <Route path="/admin/movies/comments" element={<AllComments />} /> */}
       </Route>
